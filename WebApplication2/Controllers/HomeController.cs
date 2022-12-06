e8fa49e62c54674e4783e9b2ee7c9a5531acdd74
swaroop_patil <swaroop.patil@myashida.com>
2022-12-06 16:59:42 +0530

    first commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string conString = "Data Source=RD_131;Initial Catalog=test;User ID=sa;Password=pass@123;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;";
+       
+
+        public IActionResult DataTable()
+        {
+            string query = "select * from tbl_student";
+            SqlConnection con = new SqlConnection(conString);
+
+            List<Student> user = con.Query<Student>(query).ToList();
+            return View(user);
+          
+
+        }
+        public IActionResult Login()
+        {
+            return View();
+
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Student s)
+        {
+            Console.WriteLine(s.Gender);
+            string query = $"insert into tbl_student(Name, Email, Gender, Location,Password) values (@Name, @Email, @Gender, @Location, @Password)";
+            SqlConnection con = new SqlConnection(conString);
+            con.Execute(query, s);
+            return Redirect("/Home/DataTable");
+
+        }   
+        public IActionResult Update()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }    
+            
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Location { get; set; }
+        public string Password { get; set; }    
+    }
+}
